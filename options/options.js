e6665b9fd7775d613f4648cb3141af4eba8bd8ad
Crowdbotics <team@crowdbotics.com>
2023-05-26 05:28:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-sea-41009",
+  url: "https://white-sea-41009.botics.co",
+  api: "https://white-sea-41009.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
